bdcc116059d6ebc41722adecd6473df32046489b
Paul <paulalexrees@gmail.com>
2016-10-18 13:31:17 +0100

    basic transaction storage

--- lib/account.rb
+++ lib/account.rb
@@ -1,9 +1,26 @@
 class Account
 
-  attr_reader :balance
+  attr_reader :balance, :transactions
 
   def initialize(balance = 0)
     @balance = balance
+    @transactions = []
+  end
+
+  def deposit(amount)
+    @balance += amount
+    @transactions << {deposit:amount, balance:balance}
+    balance
+  end
+
+  def withdraw(amount)
+    @balance -= amount
+    @transactions << {withdrawal:amount, balance:balance}
+    balance
+  end
+
+  def print_statement
+  
   end
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,8 +2,38 @@ describe Account do
 
   let(:account){ described_class.new }
 
-  it "has a balance" do
-    expect(account.balance).to be_a Integer
+  context "init" do
+
+    it "initializes with 0 balance as default" do
+      expect(account.balance).to eq 0
+    end
+
+    it "can initialize with custom balance" do
+      expect(Account.new(1000).balance).to eq 1000
+    end
+
   end
+
+  context "transactions" do
+
+    before { account.deposit(2000) }
+
+    it "adds funds on deposit" do
+      expect(account.balance).to eq 2000
+    end
+
+    it "subtracts funds on withdrawal" do
+      account.withdraw(1000)
+      expect(account.balance).to eq 1000
+    end
+
+    it "saves each transaction" do
+      account.withdraw(1000)
+      expect(account.transactions).to eq [{deposit:2000, balance:2000}, {withdrawal:1000, balance:1000}]
+    end
+
+  end
+
+
 
 end
